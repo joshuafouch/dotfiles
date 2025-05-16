34eae07f3f04af89905679e5c18026e82b712b98
joshuafouch <joshfouch777@gmail.com>
2025-05-16 01:06:24 -0800

    added git token aliases

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,5 +1,8 @@
 # all of my aliases are stored in here
 
+alias zshconfig='nvim ~/.zshrc'
+alias aliasconfig='nvim $ZSH_CUSTOM/aliases.zsh'
+
 alias c='clear'
 alias rebterm='zsh && exit'
 
@@ -15,3 +18,7 @@ alias fricking='sudo apt-get'
 
 alias swap-esctocaps="setxkbmap -option \"caps:swapescape\""
 alias swapesc-reset="setxkbmap -option \"\""
+
+alias sudopush='xclip -sel c < ~/.gittoken.txt && git push'
+alias sudopull='xclip -sel c < ~/.gittoken.txt && git pull'
+alias sudoclone='xclip -sel c < ~/.gittoken.txt && git clone'
