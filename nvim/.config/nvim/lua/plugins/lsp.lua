35abbc0b24e1eace1c49cc346874fe3afdc722b5
joshuafouch <joshfouch777@gmail.com>
2025-06-03 21:50:59 -0800

    file org

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,48 +1,46 @@
 return {
   {
-    "williamboman/mason.nvim",
+    'williamboman/mason.nvim',
     dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
+      'williamboman/mason-lspconfig.nvim',
+      'neovim/nvim-lspconfig',
     },
     opts = {
       servers = {
-
+        -- add pre-installed lsps here:
         lua_ls = {
           settings = {
             Lua = {
               diagnostics = {
-                globals = { "vim" },
-              }
-            }
-          }
+                globals = { 'vim' },
+              },
+            },
+          },
         },
         cssls = {},
         html = {},
         clangd = {},
         bashls = {
           settings = {
-            filetypes = { "sh", "zsh" },
-          }
+            filetypes = { 'sh', 'zsh' },
+          },
         },
         rust_analyzer = {},
-        -- add more pre-installed lsps here:
         ts_ls = {},
         eslint = {},
-      }
+      },
     },
     config = function(_, opts)
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "eslint", "ts_ls"}
-      })
+      require('mason').setup()
+      require('mason-lspconfig').setup {
+        ensure_installed = { 'lua_ls', 'eslint', 'ts_ls' },
+      }
 
       -- loop through all servers list
       for server, config in pairs(opts.servers) do
-				vim.lsp.config(server, config)
-				vim.lsp.enable(server)
-			end
-
-    end
+        vim.lsp.config(server, config)
+        vim.lsp.enable(server)
+      end
+    end,
   },
 }
