5ba7dc96320aaa413dae79f7f87ab255fbd2cb2b
joshuafouch <joshfouch777@gmail.com>
2025-06-27 19:14:31 -0800

    bruih

--- zsh/.config/zsh/.aliases.zsh
+++ zsh/.config/zsh/.aliases.zsh
@@ -1,6 +1,7 @@
 # my custom aliases
 
 alias cd='z'	# dependent on zoxide tool
+alias p='sudo pacman'
 
 alias zshconf='nvim $ZDOTDIR/.zshrc'
 alias aliasconf='nvim $ZDOTDIR/.aliases.zsh'
@@ -10,21 +11,16 @@ alias mkdir='mkdir -p'
 alias c='clear'
 alias zsh='zsh && exit'
 
-
 alias ls='exa'	# dependent on eza tool
 alias l='exa -lah'
 alias sl='exa'
 alias cat='bat'	# dependent on bat tool
 alias v='nvim'	# dependent on neovim
-alias vim='nvim'
+alias vim='nvim'  # dependent on neovim
 
 alias swap-esctocaps="setxkbmap -option \"caps:swapescape\""
 alias swapesc-reset="setxkbmap -option \"\""
 
-alias sudopush='xclip -sel c < ~/Documents/.gittoken.txt && git push'	# dependent on xlip tool
-alias sudopull='xclip -sel c < ~/Documents/.gittoken.txt && git pull'
-alias sudoclone='xclip -sel c < ~/Documents/.gittoken.txt && git clone'
-alias token='xclip -sel c < ~/Documents/.gittoken.txt' 
 alias lg='lazygit'
 
 alias poke='pokemon-colorscripts'	# dependent on pokemon-colorscripts
