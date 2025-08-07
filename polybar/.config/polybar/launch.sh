892e98c79591a7ee58f2b8d52246ec043d3a7a71
joshuafouch <joshfouch777@gmail.com>
2025-08-06 17:41:43 -0800

    multi-monitor setup

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,11 +4,16 @@
 # If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
 # Launch bar
 echo "---" | tee -a /tmp/polybar1.log
 polybar bar 2>&1 | tee -a /tmp/polybar1.log &
 disown
+
+# Launch bar for second monitor (may have multiple names)
+if [[ $(xrandr -q | grep 'DP-4 connected') ]]; then
+    polybar bartwo &
+fi
 
 echo "Bars launched..."
